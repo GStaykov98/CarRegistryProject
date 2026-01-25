d022f8e456ec24dc43a27c156f066e418572b35d
Gstaykov98 <g.staykov98@gmail.com>
2026-01-25 13:02:01 +0200

    Added comprehensive insurance model

---
+++ Models/ComprehensiveInsurance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRegistryProject.Models
+{
+    public class ComprehensiveInsurance
+    {
+        public int Id { get; set; }
+
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public int PolicyNumber { get; set; }
+    }
+}
